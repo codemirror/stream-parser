86e497d052fdd6e472fbc2fcc56f2b11d5580fd6
Marijn Haverbeke <marijn@haverbeke.nl>
2020-12-29 13:11:39 +0100

    Add boilerplate

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import dts from "rollup-plugin-dts"
+
+export default [{
+  input: "./src/stream-parser.js",
+  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  output: {
+    format: "esm",
+    file: "./dist/index.js",
+    externalLiveBindings: false
+  }
+}, {
+  input: "./src/stream-parser.d.ts",
+  output: {
+    format: "esm",
+    file: "./dist/index.d.ts",
+  },
+  plugins: [dts()],
+  onwarn(warning, warn) { if (warning.code != "CIRCULAR_DEPENDENCY") warn(warning) }
+}]
